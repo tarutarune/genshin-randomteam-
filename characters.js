c89fc6473dd9beea0c4751066eb7283386c18b0f
tarutarune <gsp.ghd@gmail.com>
2026-05-26 10:22:23 +0900

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,1 +1,28 @@
+const characters = [
+  {
+    id: "traveler",
+    name: "旅人",
+    rarity: 5,
+    element: "風",
+    weapon: "片手剣",
+    image: "images/traveler.png"
+  },
 
+  {
+    id: "amber",
+    name: "アンバー",
+    rarity: 4,
+    element: "炎",
+    weapon: "弓",
+    image: "images/amber.png"
+  },
+
+  {
+    id: "kaeya",
+    name: "ガイア",
+    rarity: 4,
+    element: "氷",
+    weapon: "片手剣",
+    image: "images/kaeya.png"
+  }
+];
